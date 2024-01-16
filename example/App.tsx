b2f9dc2cbce2ea4d008a793598e700d15a57006c
Nguyen Thinh <thinhnn@Nguyens-Mac-mini-3.local>
2024-01-16 18:15:08 +0700

    chore: init example app

---
+++ example/App.tsx
@@ -0,0 +1,24 @@
+/* eslint-disable react/self-closing-comp */
+/* eslint-disable react-native/no-inline-styles */
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ */
+
+import React from 'react';
+import {SafeAreaView, View} from 'react-native';
+
+function App(): JSX.Element {
+  return (
+    <SafeAreaView style={{flex: 1, backgroundColor: 'white'}}>
+      <View
+        style={{
+          flex: 1,
+        }}></View>
+    </SafeAreaView>
+  );
+}
+
+export default App;
